32ff13ee643e12e07c55b08869391796e03c51a8
Bohnna Chhim <info@bohnna.com>
2015-07-25 12:12:16 -0400

    Init

---
+++ Board.java
@@ -0,0 +1,241 @@
+import java.util.*;
+/**
+ * Write a description of class Board here.
+ * 
+ * @author BUNZ 
+ * @version 1.BUNZ
+ */
+public class Board {
+    char [][] gameBoard;
+   
+    public Board() {
+        gameBoard = new char[3][3];
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                gameBoard[row][col] = ' ';
+            }
+        }
+    }
+   
+    public boolean checkForWinner (char marker) {
+        boolean isAWinner = false;
+        int consecutive;
+        int col;
+        int row;
+       
+        consecutive = 0;
+        for (row = 0; row < 3 && consecutive != 3; row++) {
+            consecutive = 0;
+
+            for (col = 0; col < 3; col ++) {
+                if (gameBoard[row][col] == marker) {
+                    consecutive++;
+                }
+            }
+
+        }
+
+        if (consecutive == 3) {
+            isAWinner = true;
+        }
+
+        if (!isAWinner) {
+
+            if (gameBoard[1][1] == marker) {
+                for (row = 0; row < 3; row += 2) {
+                    if (gameBoard[row][0] == marker) {
+                        if (row == 0 && gameBoard[2][2] == marker) {
+                            isAWinner = true;
+                            break;
+                        } else if (row == 2 && gameBoard[0][2] == marker) {
+                            isAWinner = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!isAWinner) {
+                    for (col = 0; col < 3 && consecutive != 3; col++) {
+                        consecutive = 0;
+
+                        for (row = 0; row < 3; row ++) {
+                            if (gameBoard[row][col] == marker) {
+                                consecutive++;
+                            }
+                        }
+
+                    }
+
+                    if (consecutive == 3) {
+                        isAWinner = true;
+                    }
+                }
+            }
+        }
+
+        return isAWinner;
+    } 
+    
+    public void computerMove(char marker, char humanMarker) {
+
+        boolean canComputerWin = false;
+        boolean canHumanWin = false;
+        Board copyBoard;
+        int row;
+        int col;
+        int tryRow;
+        int tryCol;
+        row = 0;
+        col = 0;
+        int[] humanWin = checkForWin(humanMarker);
+        int[] computerWin = checkForWin(marker);
+
+        if (computerWin[0] > 0) {
+            System.out.println("Potential win detected at " + computerWin[0] + "x" + computerWin[1] + ". Going for gold!");
+            System.out.println(Arrays.toString(computerWin));
+            gameBoard[computerWin[0]-1][computerWin[1]-1] = marker;
+        } else if (humanWin[0] > 0) {
+            System.out.println("Potential win detected at " + humanWin[0] + "x" + humanWin[1] + ". Initiating Block!");
+            System.out.println(Arrays.toString(humanWin));
+            gameBoard[humanWin[0]-1][humanWin[1]-1] = marker;
+        } else {
+            System.out.println("No wins detected. Randomizing spot...");
+
+            boolean placed  = false;
+            Random rand     = new Random();
+            
+            do {
+                tryRow = rand.nextInt(3);
+                tryCol = rand.nextInt(3);
+                int pickedRow = tryRow + 1;
+                int pickedCol = tryCol + 1;
+                // System.out.println("Not placed, Trying " + pickedRow + "x" + pickedCol);
+
+                if (gameBoard[tryRow][tryCol] == ' ') {
+
+                    gameBoard[tryRow][tryCol] = marker;
+                    placed = true;
+                    // System.out.println("Placed");
+                }
+
+            } while(!placed);
+          
+        }
+      
+    }
+
+    private int[] checkForWin(char marker) {
+      boolean canWin = false;
+      Board copyBoard;
+      int row;
+      int col;
+      row = 0;
+      col = 0;
+      int[] winPosition = new int[2];
+      
+
+      for (row = 0; row < 3 && !canWin; row++) {
+          for(col = 0; col < 3 && !canWin; col++) {
+
+              if (gameBoard[row][col] == ' ') {
+                  copyBoard           = copyTheBoard();
+                  copyBoard.makeMove(row, col, marker);
+                  canWin              = copyBoard.checkForWinner(marker);
+                  if (canWin) {
+                    System.out.println("Win Detected at " + row + "x" + col);
+                    winPosition[0] = row+1;
+                    winPosition[1] = col+1;
+                  }
+              }
+          }
+      }
+
+      if (canWin) {
+
+      } else {
+
+          for (col = 0; col < 3 && !canWin; col++) {
+              for (row = 0; row < 3 && !canWin; row++) {
+
+                  if (gameBoard[row][col] == ' ') {
+                      copyBoard           = copyTheBoard( );
+                      copyBoard.makeMove(row, col, marker);
+                      canWin              = copyBoard.checkForWinner(marker);
+                      if (canWin) {
+                          System.out.println("Win Detected at " + row + "x" + col);
+                          winPosition[0] = row+1;
+                          winPosition[1] = col+1;
+                      }
+                  }
+
+              }
+          }
+      }
+
+      return winPosition;
+
+    }
+
+    public boolean humanMoveOK(int row, int col) {
+        if (row > 0 && row <= 3 && col > 0 && col <= 3 && gameBoard[row-1][col-1] == ' ') {
+          return true;
+        } else {
+          System.out.print("Invalid selection. Try again. \n");
+          return false;
+        }
+    }
+    
+    public void makeMove(int row, int col, char marker) {
+        gameBoard[row][col] = marker;
+    }
+            
+    private Board copyTheBoard() {
+      Board temp;
+      temp = new Board();
+
+        for (int row = 0; row < 3; row++) {
+          for (int col = 0; col < 3; col++) {
+              temp.makeMove(row,col, gameBoard[row][col]);
+          }
+        }
+
+        return temp;
+    }
+     
+    public boolean humanMove(int row, int col, char marker) {
+        boolean placed;
+
+        if (row > 3 || col > 3) {
+            System.out.println("Invalid number"); 
+            placed = false;
+
+        } else if (gameBoard[row-1][col-1] == ' ') {
+                gameBoard[row-1][col-1] = marker;
+                placed = true;
+
+        } else {
+            placed = false;
+        }
+
+        return placed;
+    }     
+     
+    public String toString( ) {
+        String result = "";
+        int row;
+        int col;
+        result += "    1  2   3\n";
+
+        for(row = 0; row < 3; row++) {
+            result += "\n" + (row+1);
+
+            for (col = 0; col < 3; col++) {
+                result += "  " + gameBoard[row][col] + " ";
+            }
+          
+
+        }
+        return result;
+    }     
+}
---
+++ Game.java
@@ -0,0 +1,102 @@
+/* Write your documentation here
+ * 
+ */
+import java.util.*;
+
+public class Game
+{
+    public static void main(String[ ] args)
+    {
+        Scanner in;
+        Random rand;
+        char choice;
+        int toss;
+        int row;
+        int col;
+        int turns;
+        boolean winner;
+        char winMarker;
+        
+        Board tictactoe;
+        
+        final char humanMarker;
+        final char computerMarker;
+        
+        in        = new Scanner(System.in);
+        winMarker = ' ';
+        rand      = new Random( );
+        winner    = false;
+        turns     = 0;
+        tictactoe = new Board( );
+        
+        System.out.println("Pick (h)eads or (t)ails");
+
+        choice    = in.next( ).charAt(0 );
+        toss      = rand.nextInt(2);
+
+        if (toss == 1 && choice == 'h') {
+            humanMarker     = 'X';
+            computerMarker  = 'O';
+        } else {
+            humanMarker     = 'O';
+            computerMarker  = 'X';
+        }
+
+        System.out.println("You are " + humanMarker + " and the computer is " + computerMarker);
+      
+        if (humanMarker == 'X') {
+            do {
+                System.out.print("Enter a row 1 to 3: ");
+                row = in.nextInt();
+
+                System.out.print("Enter a column 1 to 3: ");
+                col = in.nextInt();
+            } while(!tictactoe.humanMoveOK(row, col));
+
+            tictactoe.makeMove(row-1, col-1, humanMarker);
+        }
+
+        System.out.println(tictactoe);
+        turns++;
+        
+        while(!winner) {
+            tictactoe.computerMove(computerMarker, humanMarker);
+            winner = tictactoe.checkForWinner(computerMarker);
+            System.out.println("____________________________\n");
+            turns++;
+
+            if (winner) {
+                winMarker = computerMarker;
+            } else if (turns >= 9) {
+                System.out.println("Cat. I'll get you next time.....");
+                System.exit(0);
+            } else {
+                do {
+                    System.out.println(tictactoe);
+                    System.out.print("Enter a row 1 to 3: ");
+                    row = in.nextInt( );
+                    System.out.print("Enter a column 1 to 3: ");
+                    col = in.nextInt( );
+                } while(!tictactoe.humanMoveOK(row, col));
+
+                tictactoe.makeMove(row-1, col-1, humanMarker);
+                turns++;
+                winner = tictactoe.checkForWinner(humanMarker);
+
+                if (winner) {
+                    winMarker = humanMarker;
+                } else if (turns >= 10) {
+                    System.out.println("TIE! TRY AGAIN DAWGS");
+                    System.exit(0);
+                }
+            }
+          
+        } // end of while loop for game 
+
+        System.out.println("___________________________");
+        System.out.println(tictactoe + "\n");
+        System.out.println("***********\n      " +winMarker + " wins!");
+       
+    } // end of main()
+        
+}
